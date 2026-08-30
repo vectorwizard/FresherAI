baea6e92435d2de7536c93374ee4df476fed835f
Arijit <arijitroylochan@gmail.com>
2026-08-30 19:57:14 +0530

    Built Dashboard Page

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import { useNavigate } from 'react-router-dom'
+import api from '../utils/axios'
+import { motion } from "motion/react"
+import { FiSidebar } from 'react-icons/fi'
+import { useEffect } from 'react'
 
-const Dashboard = ({user, setuser}) => {
+const Dashboard = ({ user, setuser }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(true)
+  const [mobileOpen, setMobileOpen] = useState(false)
+  const navigate = useNavigate()
+  const handleLogout = async () => {
+    try {
+      const res = await api.post("/api/auth/logout")
+
+      if (res.data.success) {
+        setuser(null)
+        navigate("/")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div className='text-blue-700 text-3xl'>
-      {user.name}
+    <div className=''>
+      <Sidebar user={user}
+        onNewInterview={() => navigate("/interview")}
+        onLogout={handleLogout}
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        mobileOpen={mobileOpen}
+        setMobileOpen={setMobileOpen}
+      />
+
+      <motion.main className={`flex-1 min-h-screen px-3 sm:px-4 md:px-6 py-4 md:py-6 transition-all duration-300 ${sidebarOpen ? "md:ml-[260px]" : "md:ml-[72px]"
+        }`}>
+        {/* top Area */}
+        <div className='flex items-center justify-between mb-5 md:mb-6'>
+          <div className='flex items-center gap-2.5'>
+
+            <motion.button
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              onClick={() => setMobileOpen(true)}
+              className='md:hidden text-black/40 hover:text-[#0A0A0A] transition-colors'>
+              <FiSidebar size={17} />
+            </motion.button>
+
+            <motion.div
+              initial={{ opacity: 0, y: -12 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.4 }}
+            >
+              <p className='text-black/40 text-[11px] md:text-xs font-medium mb-0.5'>Overview</p>
+
+              <h2 className='text-lg md:text-xl font-bold text-[#0A0A0A]'>Hello, {user?.name?.split(" ")[0]} 👋 </h2>
+            </motion.div>
+
+          </div>
+        </div>
+
+        <div className='h-px bg-black/8 mb-5 md:mb-6' />
+
+      </motion.main>
+
     </div>
   )
 }
