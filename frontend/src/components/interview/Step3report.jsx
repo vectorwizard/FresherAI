7459eab69d7d89efce41f7fd08a647b96d5367fb
Arijit <arijitroylochan@gmail.com>
2026-09-04 23:07:22 +0530

    built 2nd step of interviewpage

--- frontend/src/components/interview/Step3report.jsx
+++ frontend/src/components/interview/Step3report.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Step3report = ({report, user, setuser}) => {
   return (
     <div>
-      
+      {user.name}
     </div>
   )
 }
--- frontend/src/pages/InterviewReport.jsx
+++ frontend/src/pages/InterviewReport.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Step3report from '../components/interview/Step3report'
+import { useNavigate, useParams } from 'react-router-dom'
+import { getInterview } from '../apis/interview.api'
 
-const InterviewReport = () => {
+function InterviewReport({user, setuser}) {
+  const {id} = useParams()
+    const [loading , setLoading] = useState(true)
+    const [report,setReport] = useState(null)
+    const navigate = useNavigate()
+
+    useEffect(()=>{
+        const fetchReport = async () => {
+            const response = await getInterview(id)
+            const data = response?.interview
+            if(data.status !== "completed"){
+                navigate(`/interview/${id}`,
+                    {replace: true});
+                    return;
+            }
+            setReport(data)
+            setLoading(false)
+         }
+
+         fetchReport()
+
+    },[id,navigate])
+
+    if(loading){
+        return(
+            <div className="min-h-screen bg-[#07000F] flex items-center justify-center">
+        <div className="w-12 h-12 rounded-full border-4 border-white/20 border-t-white animate-spin" />
+      </div>
+        )
+    }
+    if(!report) return null
+
+
   return (
-    <div>
-      
-    </div>
+   <Step3report
+   user={user}
+   setuser={setuser}
+   report={report}
+   />
   )
 }
 
