16ae8478b5a00d591637f9635bf6480363f6a471
Arijit <arijitroylochan@gmail.com>
2026-09-09 01:53:40 +0530

    bug fixes

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -15,7 +15,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://fresher-ai-two.vercel.app",
     credentials: true
 }))
 
