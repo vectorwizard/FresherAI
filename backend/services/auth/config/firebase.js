2bf393e8a419a5235f31301fe17beea5c7da9388
Arijit <arijitroylochan@gmail.com>
2026-09-09 01:12:40 +0530

    changed firebase.js

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,7 +1,9 @@
 import { cert, initializeApp } from "firebase-admin/app";
 
-import serviceAccount from "../serviceAccountKey.json" with {type: "json"}
-
 export const app = initializeApp({
-    credential: cert(serviceAccount)
-})
+    credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    })
+});
