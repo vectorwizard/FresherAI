bbfe34b1f3dcdce9b1c69c5eb48530aebcfd6645
Arijit <arijitroylochan@gmail.com>
2026-09-09 01:50:02 +0530

    changed firebase.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true
-})
+});
 
 export default api;
